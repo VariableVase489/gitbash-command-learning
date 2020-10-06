e5ff288d08a2640ed327d9c90d070f2d4265ec99
VariableVase489 <aidancappy73@gmail.com>
2020-10-05 21:56:41 -0500

    modified sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, box2, box3, box4, pig1, pig2, log1;
 var backgroundImg,platform;
 
 function preload() {
@@ -26,7 +26,7 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig2 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -52,7 +52,7 @@ function draw(){
 
     box3.display();
     box4.display();
-    pig3.display();
+    pig2.display();
     log3.display();
 
     box5.display();
